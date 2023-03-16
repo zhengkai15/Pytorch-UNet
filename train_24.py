4273d8fac469ed5eac6e12e1f4434e0b78e5e1be
Sky <156252108@qq.com>
2023-03-16 22:24:11 +0800

    fix parallel bug

---
+++ train_24.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/3/15 9:52 下午
+# @Author  : Kai Zheng
+# @FileName: train_24.py
+# @Software: PyCharm
+# @Email: 156252108@qq.com
+
+# code is far away from bugs with the god animal protecting
+'''
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛ 
+'''
+
+import argparse
+import logging
+import os
+import random
+import sys
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+import torchvision.transforms.functional as TF
+from pathlib import Path
+from torch import optim
+from torch.utils.data import DataLoader, random_split
+from tqdm import tqdm
+
+import wandb
+from evaluate import evaluate
+from unet import UNet
+from utils.data_loading import BasicDataset, CarvanaDataset
+from utils.dice_score import dice_loss
+
+dir_img = Path('../训练集/train/1471/')
+dir_mask = Path('../训练集/train/1471/')
+dir_checkpoint = Path('./checkpoints/')
+
+
+def train_model(
+        model,
+        device,
+        epochs: int = 5,
+        batch_size: int = 1,
+        learning_rate: float = 1e-5,
+        val_percent: float = 0.1,
+        save_checkpoint: bool = True,
+        img_scale: float = 0.5,
+        amp: bool = False,
+        weight_decay: float = 1e-8,
+        momentum: float = 0.999,
+        gradient_clipping: float = 1.0,
+):
+    # 1. Create dataset
+    try:
+        dataset = CarvanaDataset(dir_img, dir_mask, img_scale)
+    except (AssertionError, RuntimeError, IndexError):
+        dataset = BasicDataset(dir_img, dir_mask, img_scale)
+
